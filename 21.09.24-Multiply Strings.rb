13eaf3d412a6eea4fe6eca1f8c1e83b65e951bff
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-09-26 09:06:08 -0600

    Update conditional to hash

--- 21.09.24-Multiply Strings.rb
+++ 21.09.24-Multiply Strings.rb
@@ -66,8 +66,8 @@ def to_integer(num)
         if num == "0"
             sum += 0
         else
-            array = ["0","1","2","3","4","5","6","7","8","9"]
-            int = array.find_index(num)
+            hash = {"1"=>1, "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9}
+            int = hash[num]
             sum += int*10**index
         end  
     end
