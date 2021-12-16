a3e79bda07d49f56d7ff9c08e70badefd8248679
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-15 19:38:33 -0700

    Solution

---
+++ 21.12.15-Codility Demo.rb
@@ -0,0 +1,39 @@
+# Pseudocode
+# quick_sort array
+# Remove duplicates
+# if sorted_array[0] > 1
+#   Return 1
+# if sorted_array[1] > 2
+#   Return 2
+# if sorted_array[2] > 3
+#   Return 3
+
+def quick_sort(array)
+  if array.length > 1
+    pivot = array.pop
+    lesser = []
+    greater = []
+    array.each do |integer|
+      if integer < pivot
+        lesser.push(integer)
+      else
+        greater.push(integer)
+      end
+    end
+    array = quick_sort(lesser) + [pivot] + quick_sort(greater)
+  end
+  array
+end
+
+def solution(a)
+  sorted_array = quick_sort(a).uniq
+  (1..100000).each do |n|
+    if sorted_array[n-1] > n
+      return n
+    end
+  end
+end
+
+a = [1, 3, 6, 4, 1, 2, 17, 5, 7]
+
+print solution(a)
