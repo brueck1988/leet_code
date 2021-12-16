ee64bb9f0a09654a825dcac404a860f762b97a9d
Alexander Brueck <73265236+brueck1988@users.noreply.github.com>
2021-12-16 10:33:42 -0700

    Initial commit

---
+++ 21.12.16-Codility Challenge-1.rb
@@ -0,0 +1,14 @@
+# Pseudocode
+
+
+def solution(a)
+
+end
+
+a = [1, 3, 6, 4, 1, 2, 17, 5, 7]
+
+time_1 = Time.now
+puts solution(a)
+time_2 = Time.now
+time_elapsed = time_2 - time_1
+puts time_elapsed
